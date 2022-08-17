2e9d4822430dd17cf5ee6d9274a7a645f554965f
cburgyan <cburgyan@gmail.com>
2022-08-17 12:49:48 -0500

    add ability to add new posts to the database

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
+import datetime
 
 
-## Delete this code:
+# # Delete this code:
 # import requests
 # posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
 
@@ -16,12 +17,13 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
-##CONNECT TO DB
+# #CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-##CONFIGURE TABLE
+
+# #CONFIGURE TABLE
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
@@ -32,27 +34,34 @@ class BlogPost(db.Model):
     img_url = db.Column(db.String(250), nullable=False)
 
 
-##WTForm
+# #WTForm
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
     author = StringField("Your Name", validators=[DataRequired()])
     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = StringField("Blog Content", validators=[DataRequired()])
+    # body = StringField("Blog Content", validators=[DataRequired()])
+    body = CKEditorField('Blog Content', validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
 
 @app.route('/')
 def get_all_posts():
+    posts = BlogPost.query.all()
+    date = datetime.datetime.now().date()
+    x = datetime.datetime.strptime(str(date), '%Y-%m-%d')
+    formatted_date = x.strftime('%B %d, %Y')
+    print(formatted_date)
     return render_template("index.html", all_posts=posts)
 
 
 @app.route("/post/<int:index>")
 def show_post(index):
-    requested_post = None
-    for blog_post in posts:
-        if blog_post["id"] == index:
-            requested_post = blog_post
+    requested_post = BlogPost.query.filter_by(id=index).first()
+    # requested_post = None
+    # for blog_post in posts:
+    #     if blog_post["id"] == index:
+    #         requested_post = blog_post
     return render_template("post.html", post=requested_post)
 
 
@@ -64,6 +73,32 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route('/edit')
+def edit_post():
+    pass
+
+
+@app.route('/new-post', methods=['GET', 'POST'])
+def create_new_blog_entry():
+    post_form = CreatePostForm()
+    if post_form.validate_on_submit():
+        date = datetime.datetime.now().date()
+        x = datetime.datetime.strptime(str(date), '%Y-%m-%d')
+        formatted_date = x.strftime('%B %d, %Y')
+        new_post = BlogPost(title=post_form.title.data,
+                            subtitle=post_form.subtitle.data,
+                            date=formatted_date,
+                            body=post_form.body.data,
+                            author=post_form.author.data,
+                            img_url=post_form.img_url.data)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('get_all_posts'))
+
+    return render_template('make-post.html', form=post_form)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
